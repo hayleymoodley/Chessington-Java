5c04c06dd84537d0b11847efdf4a30788329de40
hayleymoodley <moodley.hayley@gmail.com>
2020-09-24 15:31:46 +0100

    Completed Second Challege - Passed both tests

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -19,23 +19,49 @@ public class Pawn extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> allowedMoves = new ArrayList<>();
-        //CODE FOR THE FIRST PART OF CHALLENGE - 1
-        //Coordinates to = new Coordinates(from.getRow() - 1, from.getCol());
-        //allowedMoves.add(new Move(from, to));
-        //return allowedMoves;
-        if(PlayerColour.WHITE == this.getColour()){
-            Coordinates to = new Coordinates(from.getRow()-1, from.getCol());
+
+        //CODE FOR THE SECOND PART OF CHALLENGE -2
+        if (PlayerColour.WHITE == this.getColour()) {
+            Coordinates to = new Coordinates(from.getRow() - 2, from.getCol());
             allowedMoves.add(new Move(from, to));
-        }
-        else if(PlayerColour.BLACK == this.getColour()){
-            Coordinates to = new Coordinates(from.getRow()+1, from.getCol());
+        } else if (PlayerColour.BLACK == this.getColour()) {
+            Coordinates to = new Coordinates(from.getRow() + 2, from.getCol());
             allowedMoves.add(new Move(from, to));
         }
         return allowedMoves;
 
     }
 
-
 }
+
+
+
+
+
+//        //CODE FOR THE FIRST PART OF CHALLENGE - 1
+//        Coordinates to = new Coordinates(from.getRow() - 2, from.getCol());
+//        allowedMoves.add(new Move(from, to));
+//        return allowedMoves;
+
+        //CODE FOR THE FIRST PART OF CHALLENGE - 2
+//        Coordinates to = new Coordinates(from.getRow() - 2, from.getCol());
+//        allowedMoves.add(new Move(from, to));
+//        return allowedMoves;
+
+//         //CODE FOR THE SECOND PART OF CHALLENGE -1
+//        if(PlayerColour.WHITE == this.getColour()){
+//            Coordinates to = new Coordinates(from.getRow()-1, from.getCol());
+//            allowedMoves.add(new Move(from, to));
+//        }
+//        else if(PlayerColour.BLACK == this.getColour()){
+//            Coordinates to = new Coordinates(from.getRow()+1, from.getCol());
+//            allowedMoves.add(new Move(from, to));
+//        }
+//        return allowedMoves;
+//
+//    }
+
+
+
 
 
